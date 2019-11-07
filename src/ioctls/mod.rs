652eb11dc0314d8800cb2bb25cc39ff1ad9367e4
Adrian Catangiu <acatan@amazon.com>
2019-11-07 12:40:36 +0200

    use safe wrappers Msrs and MsrList from kvm-bindings
    Refactor the unsafe code concerning kvm_msr_list and kvm_msrs,
    to work with the safe fam-wrappers MsrList and Msrs.
    
    Signed-off-by: Adrian Catangiu <acatan@amazon.com>

--- src/ioctls/mod.rs
+++ src/ioctls/mod.rs
@@ -6,8 +6,6 @@
 // found in the THIRD-PARTY file.
 
 use std::io;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-use std::mem::size_of;
 use std::os::unix::io::AsRawFd;
 use std::ptr::null_mut;
 use std::result;
@@ -28,39 +26,6 @@ pub mod vm;
 /// This typedef is generally used to avoid writing out io::Error directly and
 /// is otherwise a direct mapping to Result.
 pub type Result<T> = result::Result<T, io::Error>;
-
-// Returns a `Vec<T>` with a size in bytes at least as large as `size_in_bytes`.
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn vec_with_size_in_bytes<T: Default>(size_in_bytes: usize) -> Vec<T> {
-    let rounded_size = (size_in_bytes + size_of::<T>() - 1) / size_of::<T>();
-    let mut v = Vec::with_capacity(rounded_size);
-    for _ in 0..rounded_size {
-        v.push(T::default())
-    }
-    v
-}
-
-// The kvm API has many structs that resemble the following `Foo` structure:
-//
-// ```
-// #[repr(C)]
-// struct Foo {
-//    some_data: u32
-//    entries: __IncompleteArrayField<__u32>,
-// }
-// ```
-//
-// In order to allocate such a structure, `size_of::<Foo>()` would be too small because it would not
-// include any space for `entries`. To make the allocation large enough while still being aligned
-// for `Foo`, a `Vec<Foo>` is created. Only the first element of `Vec<Foo>` would actually be used
-// as a `Foo`. The remaining memory in the `Vec<Foo>` is for `entries`, which must be contiguous
-// with `Foo`. This function is used to make the `Vec<Foo>` with enough space for `count` entries.
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn vec_with_array_field<T: Default, F>(count: usize) -> Vec<T> {
-    let element_space = count * size_of::<F>();
-    let vec_size_bytes = size_of::<T>() + element_space;
-    vec_with_size_in_bytes(vec_size_bytes)
-}
 
 /// Safe wrapper over the `kvm_run` struct.
 ///
