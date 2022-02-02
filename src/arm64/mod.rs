e562f49e602290fae5a353bc90d21115e4834710
Sergii Glushchenko <gsserge@amazon.com>
2022-02-02 11:37:21 +0100

    Fix warnings and adjust test coverage
    Signed-off-by: Sergii Glushchenko <gsserge@amazon.com>

--- src/arm64/mod.rs
+++ src/arm64/mod.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #[allow(clippy::all)]
+// Keep this until https://github.com/rust-lang/rust-bindgen/issues/1651 is fixed.
+#[cfg_attr(test, allow(deref_nullptr))]
 pub mod bindings;
 #[cfg(feature = "fam-wrappers")]
 pub mod fam_wrappers;
