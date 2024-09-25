adaf1ee6ad0751911b175bc38cfd1a17abac7019
Ruoqing He <heruoqing@iscas.ac.cn>
2024-09-25 02:44:24 +0000

    build: Fix unexpected attribute warning
    Add `has_sev` to expected attribute.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 fn main() {
+    // Add `has_sev` to expected attributes.
+    println!("cargo:rustc-check-cfg=cfg(has_sev)");
     // Define a `has_sev` attribute, which is used for conditional
     // execution of SEV-specific tests and examples.
     if std::path::Path::new("/dev/sev").exists() {
